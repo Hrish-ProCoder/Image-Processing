ecd980e477d6474825f8cf56d45dc3b3570948e7
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-12 08:40:11 +0530

    added try except

--- Scaling an Image.py
+++ Scaling an Image.py
@@ -6,3 +6,6 @@ try:
   (height, width) = img.shape[:2]
   res = cv2.resize(img, (int(width/2), int(height/2)), interpolation = cv2.INTER_CUBIC)
   cv2.imwrite('outputimg.jpg', res)
+  
+except IOError:
+  print("Error while reading the file")
