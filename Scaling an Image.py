1c67c93f8e4a5f70b448b09e66c8f3b7a024487e
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-12 08:37:10 +0530

    Update Scaling an Image.py

--- Scaling an Image.py
+++ Scaling an Image.py
@@ -4,4 +4,4 @@ name = 'smartwatch.jpg'
 try:
   img = cv2.imread(name)
   (height, width) = img.shape[:2]
-  
+  res = cv2.resize(img, (int(width/2), int(height/2)), interpolation = cv2.INTER_CUBIC)
