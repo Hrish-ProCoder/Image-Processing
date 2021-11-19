d35d13c3b0add5596cf3ac19de235660a65f0a41
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-19 08:18:21 +0530

    Update Translating an Image.py

--- Translating an Image.py
+++ Translating an Image.py
@@ -5,11 +5,13 @@ M = np.float32([[1, 0, 100], [0, 1, 50]])
 
 # try block
 try:
+  # read image
   img = cv2.imread(FILE)
   (rows, cols) = img.shape[:2]
   
   # warpAffine - does appropriate shifting given in the translation matrix
   res = cv2.warpAffine(img, M, (cols, rows))
+  #write image
   cv2.imwrite('output.jpg', res)
   
 # except block
