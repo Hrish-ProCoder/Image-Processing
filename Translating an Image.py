7a548fc5e2264b7924f6aca16c4f083ac471bc45
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-19 08:16:56 +0530

    Update Translating an Image.py

--- Translating an Image.py
+++ Translating an Image.py
@@ -3,6 +3,7 @@ import numpy as np
 FILE = 'smartwatch.jpg'
 M = np.float32([[1, 0, 100], [0, 1, 50]])
 
+# try block
 try:
   img = cv2.imread(FILE)
   (rows, cols) = img.shape[:2]
@@ -11,5 +12,6 @@ try:
   res = cv2.warpAffine(img, M, (cols, rows))
   cv2.imwrite('output.jpg', res)
   
+# except block
 except IOError:
   print ("Error while reading files")
