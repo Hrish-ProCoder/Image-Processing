01ecd8466c8508e9911145d608b7af667486503c
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-21 09:14:24 +0530

    Update Edge Detection in an Image.py

--- Edge Detection in an Image.py
+++ Edge Detection in an Image.py
@@ -6,6 +6,9 @@ FILE_NAME = "smartwatch.jpg"
 try:
   # Read image from disk
   img = cv2.imread(FILE_NAME)
+  
    # Canny Edge Detection
    edges = cv2.Canny(img, 100, 200)
    # Write image back to disk
+    
+   cv2.imwrite('output.jpg', edges)
