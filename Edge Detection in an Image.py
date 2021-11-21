237178a3bad2b4644691d2f08d71bc27561c4a0d
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-21 09:18:15 +0530

    Edge detect via using canny feature

--- Edge Detection in an Image.py
+++ Edge Detection in an Image.py
@@ -3,6 +3,7 @@ import numpy as np
 
 FILE_NAME = "smartwatch.jpg"
 
+# try block
 try:
   # Read image from disk
   img = cv2.imread(FILE_NAME)
@@ -13,5 +14,6 @@ try:
     
    cv2.imwrite('output.jpg', edges)
 
+# except block
 except IOError:
   print ("Error while reading files.")
